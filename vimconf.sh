d31b2ecf317eb4e004c7c7c058805c9401cf7b67
Luís Humberto <47750295+betolhcs@users.noreply.github.com>
2022-09-21 14:55:24 -0300

    Mais plugins utilizados

--- vimconf.sh
+++ vimconf.sh
@@ -28,19 +28,31 @@ git clone https://github.com/tpope/vim-fugitive.git
 cd ../..
 mkdir -p nerdcommenter/start
 cd nerdcommenter/start
-git clone https://github.com/preservim/nerdcommenter.git 
+git clone https://github.com/preservim/nerdcommenter.git
 cd ../..
 mkdir -p fzf/start
 cd fzf/start
-git clone https://github.com/junegunn/fzf.vim.git 
+git clone https://github.com/junegunn/fzf.vim.git
 cd ../..
 mkdir -p polyglot/start
 cd polyglot/start
-git clone https://github.com/sheerun/vim-polyglot.git 
+git clone https://github.com/sheerun/vim-polyglot.git
 cd ../..
-mkdir -p indent-guides/start
-cd indent-guides/start
-git clone https://github.com/nathanaelkane/vim-indent-guides.git
+mkdir -p delimitmate/start
+cd delimitmate/start
+git clone https://github.com/Raimondi/delimitMate.git
+cd ../..
+mkdir -p emmet/start
+cd emmet/start
+git clone https://github.com/mattn/emmet-vim.git
+cd ../..
+mkdir -p gitgutter/start
+cd gitgutter/start
+git clone https://github.com/airblade/vim-gitgutter.git
+cd ../..
+mkdir -p indentline/start
+cd indentline/start
+git clone https://github.com/Yggdroot/indentLine.git
 cd ../..
 mkdir -p nerdtree/start
 cd nerdtree/start
